82fbddacb860d08a6f43d34170f931e196538e78
Sascha Arthur <sascha.arthur@gmail.com>
2023-02-15 21:58:02 +0100

    - remove echo

--- src/tests/SellerRequestTest.php
+++ src/tests/SellerRequestTest.php
@@ -68,7 +68,6 @@ class SellerRequestTest extends AbstractTest
         $seller = "ALYY2EOZQ7QUK";
         $request = Request::getSellerRequest(self::LOCALE, [$seller], true);
 
-        echo json_encode($request);
         $response = $this->api->sendRequestWithRetry($request);
         self::assertEquals($response->status, "OK");
         self::assertNotNull($response->sellers);
