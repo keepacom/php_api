1a8f21df46146e158a7cf6497c0797d70360a09a
sascha arthur <sascha.arthur@gmail.com>
2025-01-29 17:26:36 +0100

    - catchup with java 1.93

---
+++ src/helper/UnitCountObject.php
@@ -0,0 +1,20 @@
+<?php
+namespace Keepa\helper;
+
+class UnitCountObject
+{
+    /**
+     * @var double
+     */
+    public $unitValue;
+
+    /**
+     * @var string
+     */
+    public $unitType;
+
+    /**
+     * @var double
+     */
+    public $eachUnitCount;
+}
